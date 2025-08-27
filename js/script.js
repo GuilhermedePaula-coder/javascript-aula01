2d6af49c796596384389c1d223f92b2a8101d07b
Guilherme de Paula <guilhermekuskowski0@gmail.com>
2025-08-26 22:00:46 -0300

    criando variáveis

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@
 // alert(5+5);
 // console.log("Isso é um Console.log");
 
-let a,b,c
+/*let a,b,c
 a=5;
 b=6;
 c=7;
-let pessoa = "Guilherme"
+let pessoa = "Guilherme"*/
+
+var a,b,c;
+
+a = 3;
+b = 2;
+c = a + b;
+
+alert(c);
+
